97985b15fc31efbd4099068c5de379840771080a
Tobias Dorra <public.tobias@outlook.com>
2016-04-14 12:47:11 +0200

    Dateien angelegt

---
+++ src/commands/startup.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var routes = require('../restfull-api/routes.js');
+
+var app = express();
+app.use('/api/v1', routes);
+app.listen(8080, function(){
+	console.log('Server is listening at port 8080.');
+});
---
+++ src/core/DO-NOT-DELETE-ME.markdown
@@ -0,0 +1,1 @@
+This file prevents git from removing this currently empty folder.
