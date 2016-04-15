7f7d08a283e9c7f1f4e664e3447aa078c803fa89
Tobias Dorra <public.tobias@outlook.com>
2016-04-15 15:31:11 +0200

    Changed the configuration handling to reflect Arweds recommendations

--- src/utils/clone.js
+++ src/utils/clone.js
@@ -3,11 +3,15 @@
  * A deep copy will be performed on the given value. The copy will be returned.
  */
 var clone = function(value) {
-	// This ugly way of creating a deep copy of something 
-	// was not my own invention!!!!
-	// It actually seems to be quite comon in the 
-	// strange javascript world to do it like this.
-	return JSON.parse(JSON.stringify(value));
+	if (value === undefined) {
+		return undefined;
+	} else {
+		// This ugly way of creating a deep copy of something 
+		// was not my own invention!!!!
+		// It actually seems to be quite comon in the 
+		// strange javascript world to do it like this.
+		return JSON.parse(JSON.stringify(value));		
+	}
 };
 
 module.exports = clone;
